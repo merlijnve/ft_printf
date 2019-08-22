398bf68be0850f020d14cec4c796b3ba2813b02f
Merlijn van Engelen <merlijn@merlijn.local>
2019-08-22 19:59:33 +0200

    Added precision test to test_file

--- test_file.c
+++ test_file.c
@@ -23,6 +23,7 @@ int		csp(void)
 	s = "test";
 	c = '!';
 
+	printf("basic CSP:\n");
 	printf("real: %s, %c, %p\n", s, c, &c);
 	//ft_printf("ft_: %s, %c, %p", s, c, &c);
 	
@@ -36,6 +37,7 @@ int		diouxx(void)
 
 	s = -32767;
 	i = 'a';
+	printf("diouxX:\n");
 	printf("real: %d, %i, %o, %u, %x, %X\n", i, i, i, i, i, i);
 	//ft_printf("ft_: %d, %i, %o, %u, %x, %X\n", i, i, i, i, i, i);
 	printf("real: %hd, %hhi, %ho, %hhu, %hx, %hhX\n", s, i, s, i, s, i);
@@ -47,14 +49,25 @@ int		perc(void)
 	short	i;
 
 	i = 5;
+	printf("percentage:\n");
 	printf("real: %d%%, %%%%%i%%%%%% %o%%\n", i, i, i);
 	//ft_printf("real: %d%%, %%%%%i%%%%%% %o%%\n", i, i, i);
 	return (0);
 }
 
-int		signs(void)
+int		prec(void)
 {
-	
+	short	s;
+	float	f;
+	int		i;
+
+	s = 65;
+	f = 18.73;
+	i = 147098098;
+	printf("precision:\n");
+	printf("real: %4.1d, %8.7f, %hd\n", i, f, s);
+	//ft_printf("real: %4.1d, %8.7f, %hd\n", i, f, s);
+	return (0);
 }
 
 int		main(void)
@@ -67,5 +80,5 @@ int		main(void)
 	res[4] = perc();
 	// res[5] = signs();
 	// res[6] = fw();
-	// res[7] = prc();
+	res[7] = prec();
 }
