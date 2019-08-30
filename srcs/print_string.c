f23689c7c4f4835b23f8536f8f41f135d2450ce2
Merlijn Van engelen <mvan-eng@f1r1s9.codam.nl>
2019-08-30 15:16:25 +0200

    Changed files

--- srcs/print_string.c
+++ srcs/print_string.c
@@ -6,7 +6,7 @@
 /*   By: mvan-eng <mvan-eng@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/08/22 16:21:57 by mvan-eng       #+#    #+#                */
-/*   Updated: 2019/08/29 17:00:33 by jboer         ########   odam.nl         */
+/*   Updated: 2019/08/30 14:47:43 by mvan-eng      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ void	print_arg(t_print *print, va_list ap)
 		ft_putstr(get_va_int(print, ap));
 	if (print->fid == 'u' || print->fid == 'o' || print->fid == 'x' ||
 	print->fid == 'X')
-		ft_putstr(get_va_unsig(print, ap));
+		ft_putstr(get_va_uns(print, ap));
 	if (print->fid == 'f')
-		ft_putstr(get_float(print, ap));
+		ft_putstr(get_va_flt(print, ap));
 	if (print->fid == 'c')
-		ft_putstr(get_char(print, ap));
+		ft_putstr(get_va_chr(print, ap));
 	if (print->fid == 'p')
-		ft_putstr(get_pointer(print, ap));
+		ft_putstr(get_va_ptr(print, ap));
 	if (print->fid == 's')
-		ft_putstr(get_string(print, ap));
+		ft_putstr(get_va_str(print, ap));
 }
 
 int	print_string(t_print *print, char *fmt, va_list ap)
