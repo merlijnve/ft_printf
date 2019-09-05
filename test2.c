d4285b1b7658097b0dcaed8ed00354659fdd4472
Merlijn Van engelen <mvan-eng@f1r2s6.codam.nl>
2019-09-05 14:10:26 +0200

    CONFLICTS

--- test2.c
+++ test2.c
@@ -6,20 +6,12 @@
 /*   By: mvan-eng <mvan-eng@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/02 19:05:38 by mvan-eng       #+#    #+#                */
-<<<<<<< HEAD
-/*   Updated: 2019/09/04 12:53:03 by jboer         ########   odam.nl         */
-=======
-/*   Updated: 2019/09/05 12:45:49 by mvan-eng      ########   odam.nl         */
->>>>>>> f96804fdc7b41c7f318a21198b02fd98d92678b4
+/*   Updated: 2019/09/05 14:04:16 by mvan-eng      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-<<<<<<< HEAD
-#include "printf.h"
-=======
 #include "srcs/printf.h"
->>>>>>> f96804fdc7b41c7f318a21198b02fd98d92678b4
 
 int		main(void)
 {
