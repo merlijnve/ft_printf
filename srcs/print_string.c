dd95304da2b120086181190c090149b9762f2ad9
Joris <j.boer13@gmail.com>
2019-09-13 12:52:43 +0200

    merge

--- srcs/print_string.c
+++ srcs/print_string.c
@@ -6,7 +6,7 @@
 /*   By: mvan-eng <mvan-eng@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/08/22 16:21:57 by mvan-eng       #+#    #+#                */
-/*   Updated: 2019/09/12 12:50:09 by mvan-eng      ########   odam.nl         */
+/*   Updated: 2019/09/12 14:57:11 by jboer         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ void	print_arg(t_print *print, va_list ap)
 {
 	if (print->fid == 'd' || print->fid == 'i')
 		get_va_int(print, ap);
-	if (print->fid == 'o')
-		get_va_base();
-	// 	get_va_uns(print, ap);
+	// if (print->fid == 'o')
+	// 	get_va_base();
+	if (print->fid == 'u')
+		get_va_uns(print, ap);
 	// if (print->fid == 'f')
 	// 	get_va_flt(print, ap);
 	if (print->fid == 'c')
