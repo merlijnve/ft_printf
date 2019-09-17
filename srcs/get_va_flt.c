1fb5c095e42cecd579d949ea6d3f4fc2ddcb038d
Joris <j.boer13@gmail.com>
2019-09-17 17:57:10 +0200

    merge

--- srcs/get_va_flt.c
+++ srcs/get_va_flt.c
@@ -6,11 +6,30 @@
 /*   By: jboer <jboer@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/08/27 18:11:16 by jboer          #+#    #+#                */
-/*   Updated: 2019/09/16 19:37:52 by jboer         ########   odam.nl         */
+/*   Updated: 2019/09/17 17:56:14 by jboer         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
+
+static char		*fill_width_f(char *str, t_print *print)
+{
+	char		*buf;
+	size_t		slen;
+	char		c;
+
+	c = ' ';
+	if (print->flags[1] == 1)
+		c = '0';
+	slen = ft_strlen(str);
+	buf = ft_strnew(print->width);
+	ft_memset((void *)buf, c, print->width);
+	if (print->flags[3] == 1)
+		ft_strncpy(buf, str, slen);
+	else
+		ft_strncpy(&buf[print->width - slen], str, slen);
+	return (buf);
+}
 
 static void		flt_to_str(long double f, t_print *print)
 {
@@ -18,7 +37,14 @@ static void		flt_to_str(long double f, t_print *print)
 	char		*buf;
 
 	str = ft_fltoa(f, print->prec);
-	
+	if (print->width > (int)ft_strlen(str))
+	{
+		buf = str;
+		str = fill_width_f(str, print);
+		ft_strdel(&buf);
+	}
+	ft_putstr(str);
+	ft_strdel(&str);
 }
 
 void			get_va_flt(t_print *print, va_list ap)
