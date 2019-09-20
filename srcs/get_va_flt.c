4b25aff40c4baf5d72ec6724e3180306cca054b9
Merlijn van Engelen <merlijnvanengelen@gmail.com>
2019-09-20 15:53:37 +0200

    Merge

--- srcs/get_va_flt.c
+++ srcs/get_va_flt.c
@@ -6,7 +6,7 @@
 /*   By: jboer <jboer@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/08/27 18:11:16 by jboer          #+#    #+#                */
-/*   Updated: 2019/09/20 13:18:28 by jboer         ########   odam.nl         */
+/*   Updated: 2019/09/20 15:53:18 by mvan-eng      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,7 @@ static void		flt_to_str(long double f, t_print *print)
 
 	if (f == 1.0 / 0.0 || f != f || f == -1.0 / 0.0)
 		str = set_specials(f, print);
-	else	
+	else
 		str = ft_fltoa(f, print->prec);
 	if (print->prec == 0 && print->flags[0])
 		str = ft_straddtoend(str, ".");
